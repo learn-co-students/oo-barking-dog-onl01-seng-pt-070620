90cc543e74dda3cf359ee6aac607b4a4b1517481
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-20 16:04:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog 
+  #getter
+  def name=(dog_name)
+    @dog_name = dog_name
+  end
+  
+  #setter
+  def name
+    @dog_name
+  end
+  
+  def bark
+    puts 'woof!'
+  end
+end
