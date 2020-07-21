335489386c0c3f7238b3863ab5c2f62b9a69d98e
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-21 21:56:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog
+  def name=(dog_name)
+  @name = dog_name
+  end
+end
+
+def name
+  @name
+end 
+  
+  #Fido = Dog.new
+  #Dog.new = "Fido"
+  #puts fido.name
+  #end
+#end
+#def bark
+#end
